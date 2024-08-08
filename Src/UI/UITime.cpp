ac545fd9a9352b143d5129bb338bd4018494137e
Riru <Riru1290@yahoo.co.jp>
2024-08-08 20:36:45 +0900

    タイマーUI

--- Src/UI/UITime.cpp
+++ Src/UI/UITime.cpp
@@ -7,8 +7,8 @@ UITime::UITime() : UIBase()
 	imgColon_ = LoadGraph("Data/Img/UI/colon.png");
 	imgTimer_ = LoadGraph("Data/Img/UI/Icon_Clock.png");
 	imgFrame_ = LoadGraph("Data/Img/UI/Item5.png");
-	pos_ = { 100.0f,100.0f };
-	size_ = 0.75f;
+	pos_ = { 170.0f,70.0f };
+	size_ = 0.35f;
 	angle_ = 0.0f;
 }
 
@@ -27,38 +27,38 @@ void UITime::Draw()
 
 
 	// 表示数計算
-	static Vector2F offset = { 100.0f,0.0f };
+	static Vector2F offset = { 35.0f,0.0f };
 
 	// フレーム
 	Vector2F fData = data;
-	fData += {710.0f / 2.0f, 0.0f};
-	DrawRotaGraph(fData.x, fData.y, size_ * 1.2f, angle_, imgFrame_, true);
+	DrawRotaGraph(fData.x, fData.y, size_, angle_, imgFrame_, true);
 
 	// タイマー
 	Vector2F tData = data;
-	DrawRotaGraph(tData.x, tData.y, size_ * 1.2f, angle_, imgTimer_, true);
+	tData -= {100.0f, 10.0f};
+	DrawRotaGraph(tData.x, tData.y, size_ * 1.5f, angle_, imgTimer_, true);
 
 	float time = MyTimer.GetTime("GAME_TIME");
 	// 分
 	int mini = (int)(time / 60.0f);
 
-	data += {150.0f, 0.0f};
-	DrawRotaGraph(data.x, data.y, size_, angle_, imgs_[mini / 10], true);
-	data += offset;
-	DrawRotaGraph(data.x, data.y, size_, angle_, imgs_[mini % 10], true);
+	data -= {30.0f, 0.0f};
+	//DrawRotaGraph(data.x, data.y, size_, angle_, imgs_[mini / 10], true);
+	//data += offset;
+	DrawRotaGraph(data.x, data.y, size_ * 0.5f, angle_, imgs_[mini % 10], true);
 	data += offset;
 
 
 	// コロン
 	Vector2F cData = data;
-	DrawRotaGraph(cData.x, cData.y, size_, angle_, imgColon_, true);
+	DrawRotaGraph(cData.x, cData.y, size_ * 0.5f, angle_, imgColon_, true);
 	data += offset;
 
 	int sec = (int)(time - (float)mini * 60.0f);
 
-	DrawRotaGraph(data.x, data.y, size_, angle_, imgs_[sec / 10], true);
+	DrawRotaGraph(data.x, data.y, size_ * 0.5f, angle_, imgs_[sec / 10], true);
 	data += offset;
-	DrawRotaGraph(data.x, data.y, size_, angle_, imgs_[sec % 10], true);
+	DrawRotaGraph(data.x, data.y, size_ * 0.5f, angle_, imgs_[sec % 10], true);
 	data += offset;
 
 
